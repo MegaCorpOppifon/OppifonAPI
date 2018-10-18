23966d43b279b110b615fa9037f880f7a81386bf
Rasmus Ølholm Nielsen <erasmus1212@hotmail.com>
2018-10-18 14:19:19 +0200

    fix

--- OppifonAPI/Startup.cs
+++ OppifonAPI/Startup.cs
@@ -25,7 +25,7 @@ namespace OppifonAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var onnectionString = "ConnectionString";
+            var connectionString = "ConnectionString";
 
             services.AddDbContext<Context>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString(connectionString)));
