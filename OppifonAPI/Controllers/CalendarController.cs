c889de90106802738dc8fc3aeba9158ec0e0fa62
Rasmus Ølholm Nielsen <erasmus1212@hotmail.com>
2018-10-18 23:50:34 +0200

    Get expert calendar

--- OppifonAPI/Controllers/CalendarController.cs
+++ OppifonAPI/Controllers/CalendarController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using DAL.Factory;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OppifonAPI.DTO;
 
 namespace OppifonAPI.Controllers
 {
@@ -14,7 +15,7 @@ namespace OppifonAPI.Controllers
     [Route("api/[controller]")]
     public class CalendarController : ControllerBase
     {
-        private IFactory _factory;
+        private readonly IFactory _factory;
 
         public CalendarController(IFactory factory)
         {
@@ -24,7 +25,7 @@ namespace OppifonAPI.Controllers
         #region user
 
         [HttpGet("user/{id}")]
-        public IActionResult GetCalendarUser(Guid id)
+        public IActionResult GetUserCalendar(Guid id)
         {
             using (var unit = _factory.GetUOF())
             {
@@ -42,5 +43,47 @@ namespace OppifonAPI.Controllers
         }
 
         #endregion
+
+        #region expert
+
+        [HttpGet("expert/{id}")]
+        public IActionResult GetExpertCalendar(Guid id)
+        {
+            using (var unit = _factory.GetUOF())
+            {
+                try
+                {
+                    var calendar = new DTOCalendarPublic();
+
+                    var dbCalendar = unit.Users.GetEager(id).Calendar;
+
+                    calendar.Id = dbCalendar.Id;
+                    calendar.DaysOff = dbCalendar.DaysOff;
+                    calendar.DefaultDuration = dbCalendar.DefaultDuration;
+                    calendar.WorkDays = dbCalendar.WorkDays;
+                    calendar.Appointments = new List<DTOAppointmentPublic>();
+
+                    foreach (var appointment in dbCalendar.Appointments)
+                    {
+                        calendar.Appointments.Add(
+                            new DTOAppointmentPublic
+                            {
+                                Time = appointment.Appointment.Time,
+                                Duration = appointment.Appointment.Duration
+                            });
+                    }
+
+                    return Ok(calendar);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+
+        #endregion
+
     }
 }
---
+++ OppifonAPI/DTO/DTOAppointmentPublic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OppifonAPI.DTO
+{
+    public class DTOAppointmentPublic
+    {
+        public DateTime Time { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
