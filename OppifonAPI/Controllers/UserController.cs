c4c027eb40356c9d46cdae532535818be3761187
Rasmus Ølholm Nielsen <erasmus1212@hotmail.com>
2018-10-18 17:58:04 +0200

    Update User

--- OppifonAPI/Controllers/UserController.cs
+++ OppifonAPI/Controllers/UserController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OppifonAPI.Helpers;
 using DAL.Factory;
-using Microsoft.AspNetCore.Authorization;
+using DAL.Models;
+using DAL.Models.ManyToMany;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Internal;
 using OppifonAPI.DTO;
 
 namespace OppifonAPI.Controllers
@@ -12,17 +16,19 @@ namespace OppifonAPI.Controllers
     //[Authorize]
     [Produces("application/json")]
     [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UserController : ControllerBase
     {
         private readonly IFactory _factory;
+        private readonly UserManager<User> _userManager;
         
-        public UsersController(IFactory factory)
+        public UserController(IFactory factory, UserManager<User> userManager)
         {
             _factory = factory;
+            _userManager = userManager;
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetById(Guid id)
+        public IActionResult Get(Guid id)
         {
             using (var unit = _factory.GetUOF())
             {
@@ -33,42 +39,53 @@ namespace OppifonAPI.Controllers
             }
         }
 
-        [HttpPut]
-        public IActionResult GetByEmail([FromForm] DTOLoginUser dtoUser)
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody]DTOUpdateUser dtoUser)
         {
             using (var unit = _factory.GetUOF())
             {
-                var user = unit.Users.GetByEmail(dtoUser.Email);
-                var newDtoUser = new DTORegisterUser();
-                Mapper.Map(user, newDtoUser);
-                return Ok(newDtoUser);
+                // map dto to entity and set id
+                try
+                {
+                    var dbUser = unit.Users.Get(id);
+                    await _userManager.ChangePasswordAsync(dbUser, dtoUser.OldPassword, dtoUser.NewPassword);
+                    dbUser.FirstName = dtoUser.FirstName ?? dbUser.FirstName;
+                    dbUser.LastName = dtoUser.LastName ?? dbUser.LastName;
+                    dbUser.Email = dtoUser.Email ?? dbUser.Email;
+                    dbUser.City = dtoUser.City ?? dbUser.City;
+                    dbUser.PhoneNumber = dtoUser.PhoneNumber ?? dbUser.PhoneNumber;
+                    dbUser.Birthday = dtoUser.Birthday == default(DateTime) ? dtoUser.Birthday : dbUser.Birthday;
+                    dbUser.Gender = dtoUser.Gender ?? dbUser.Gender;
+                    dbUser.IsExpert = dtoUser.IsExpert;
+
+                    // Interest tags
+                    if (dtoUser.InterestTags.Any())
+                    {
+                        dbUser.InterestTags = new Collection<UserTag>();
+                        foreach (var interestTag in dtoUser.InterestTags)
+                        {
+                            var tag = unit.Tags.GetTagByName(interestTag) ?? new Tag { Name = interestTag };
+
+                            var userTag = new UserTag
+                            {
+                                Tag = tag,
+                                User = dbUser
+                            };
+
+                            dbUser.InterestTags.Add(userTag);
+                        }
+                    }
+                    
+                    // save 
+                    unit.Complete();
+                    return Ok(dbUser);
+                }
+                catch (AppException ex)
+                {
+                    // return error message if there was an exception
+                    return BadRequest(new { message = ex.Message });
+                }
             }
         }
-
-
-        //[HttpPut("{id}")]
-        //public IActionResult Update(Guid id, [FromBody]DTOUser dtoUser)
-        //{
-        //    using (var unit = _factory.GetUOF())
-        //    {
-        //        // map dto to entity and set id
-        //        try
-        //        {
-        //            dtoUser.Id = id;
-        //            var user = unit.Users.GetByEmail(dtoUser.Email);
-        //            Mapper.Map(dtoUser, user);
-
-        //            // save 
-        //            unit.Users.Update(user);
-        //            unit.Complete();
-        //            return Ok(user);
-        //        }
-        //        catch (AppException ex)
-        //        {
-        //            // return error message if there was an exception
-        //            return BadRequest(new { message = ex.Message });
-        //        }
-        //    }
-        //}
     }
 }
---
+++ OppifonAPI/DTO/DTOUpdateUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using DAL.Models;
+
+namespace OppifonAPI.DTO
+{
+    // ReSharper disable once InconsistentNaming
+    public class DTOUpdateUser
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string City { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime Birthday { get; set; }
+        public ICollection<string> InterestTags { get; set; }
+        public string Gender { get; set; }
+        public bool IsExpert { get; set; }
+    }
+}
