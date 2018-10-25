077b6861f5f136f7e065fd6f003319c1d5f5ece4
Rasmus Ølholm Nielsen <erasmus1212@hotmail.com>
2018-10-25 21:46:40 +0200

    Done

--- OppifonAPI/DTO/Calendar/DTOAppointment.cs
+++ OppifonAPI/DTO/Calendar/DTOAppointment.cs
@@ -4,6 +4,7 @@ namespace OppifonAPI.DTO
 {
     public class DTOAppointment
     {
+        public Guid Id { get; set; }
         public DateTime Time { get; set; }
         public TimeSpan Duration { get; set; }
         public string Name { get; set; }
