b9cd8c9837e9c451ca9747ef4bfaa616fed0487a
Rasmus Nielsen <erasmus1212@hotmail.com>
2018-12-06 09:35:33 +0100

    Change builder

--- OppifonAPI/Startup.cs
+++ OppifonAPI/Startup.cs
@@ -18,9 +18,15 @@ namespace OppifonAPI
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
-            Configuration = configuration;            
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -28,7 +34,7 @@ namespace OppifonAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var connectionString = "ConnectionString";
+            var connectionString = "SQLCONNSTR_ConnectionString";
 
             services.AddDbContext<Context>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString(connectionString)));
