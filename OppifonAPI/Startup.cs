d425f520730a38f75de8cba85abb613067940889
Rasmus Nielsen <erasmus1212@hotmail.com>
2018-12-06 09:55:29 +0100

    Rollback

--- OppifonAPI/Startup.cs
+++ OppifonAPI/Startup.cs
@@ -18,15 +18,9 @@ namespace OppifonAPI
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-            .SetBasePath(env.ContentRootPath)
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-            .AddEnvironmentVariables();
-
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
